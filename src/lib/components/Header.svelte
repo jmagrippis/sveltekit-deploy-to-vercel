3e9f5b6aef923d7f7cda21fbf8afa948cc50ecdc
Johnny Magrippis <j@magrippis.com>
2022-01-14 22:58:50 +0100

    ✨  add youtube icon

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,6 +5,7 @@
 	import Logo from './Logo.svelte'
 
 	import {setTheme, theme, Theme} from '$lib/stores/theme'
+	import YouTubeIcon from './YouTubeIcon.svelte'
 
 	const onThemeIconClick = () => {
 		const nextTheme = $theme === Theme.Dark ? Theme.Light : Theme.Dark
@@ -14,11 +15,21 @@
 
 <header class="flex items-center justify-between p-2 text-prime">
 	<a href="/"><Logo className="w-20" /></a>
-	<button on:click={onThemeIconClick}>
-		{#if $theme === Theme.Dark}
-			<DarkModeIcon className="w-8" />
-		{:else}
-			<LightModeIcon className="w-8" />
-		{/if}
-	</button>
+	<div class="flex gap-4">
+		<a
+			href="https://www.youtube.com/channel/UCm1ALyg61uhPoTnZBm7mY2g"
+			target="_blank"
+			rel="noopener noreferrer"
+			aria-label="YouTube"
+		>
+			<YouTubeIcon title="Johnny's YouTube channel" className="w-8" />
+		</a>
+		<button on:click={onThemeIconClick}>
+			{#if $theme === Theme.Dark}
+				<DarkModeIcon className="w-8" />
+			{:else}
+				<LightModeIcon className="w-8" />
+			{/if}
+		</button>
+	</div>
 </header>
